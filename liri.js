2dd99773c89f5ad2f118cf42497c1d2d841571b0
Garrett Griffin <griffin.garrett@gmail.com>
2019-04-28 18:02:23 -0500

    finished bandsintown get request function

--- liri.js
+++ liri.js
@@ -1,9 +1,47 @@
 require("dotenv").config();
 
-var keys = require('./keys.js')
+const keys = require('./keys.js');
+const axios = require('axios');
 
-/* var spotify = new Spotify(keys.spotify);
- */
-var axios = require('axios');
+const command = process.argv[2];
+const search = process.argv.slice(3).join(" ");
+let bandsURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
+var spotifyURL = 'https://api.spotify.com/v1/?query=' + search + '\u0026offset=0\u0026limit=20\u0026type=track';
 
-console.log('Working')
+
+if (command === 'concert-this') {
+    axios
+        .get(bandsURL)
+        .then(function (response) {
+            let jsonData = response.data;
+            
+            for(let i = 0; i < jsonData.length; i++) {
+            let showData = [
+                console.log('////////////////////////////'),
+                'Venue: ' + jsonData[i].venue.name,
+                'Location: ' + jsonData[i].venue.city,
+                'Date: ' + jsonData[i].datetime,
+                console.log('////////////////////////////'),
+            ].join('\n\n');
+            
+            console.log(showData);
+        }
+        })
+} else if (command === 'spotify-this-song') {
+    axios
+        .get(spotifyURL)
+        .then(function (response) {
+            let jsonData = response.data;
+            console.log(jsonData);
+            /* for (let i = 0; i < jsonData.length; i++) {
+                let artistData = [
+
+                ]
+            } */
+        })
+};
+
+
+
+console.log('Command: ' + command);
+console.log('Search: ' + search);
